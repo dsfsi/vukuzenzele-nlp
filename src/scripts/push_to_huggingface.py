755e64bbb7f75956f1c90345478b813b20841f6c
lastrucci01 <u20430168@tuks.co.za>
2023-12-06 12:17:24 +0200

    push_to_hub script update for mono

--- src/scripts/push_to_huggingface.py
+++ src/scripts/push_to_huggingface.py
@@ -7,40 +7,35 @@ from datasets import Dataset, DatasetDict
 REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
 DATA_PATH = Path(REPO_PATH / "data")
 ALIGNED_PATH = Path(DATA_PATH / "opt_aligned_out")
+MONO_PATH = Path(DATA_PATH / "monolingual_jsonl")
 OUT_PATH = Path(DATA_PATH / "vukuzenzele-sentence-aligned")
 
 
 if __name__ == "__main__":
-    aligned_paths = os.listdir(ALIGNED_PATH)
+    aligned_paths = os.listdir(MONO_PATH)
     aligned_paths.sort()
     token = os.environ.get('HFTOKEN')
-    print(len(aligned_paths))
+    big_dict = {}
     for i, path in enumerate(aligned_paths):
-        if i <= 48: continue
-        src_tgt_list = path.split("-")
-        src = src_tgt_list[1]  # First element
-        tgt = src_tgt_list[2].split(".")[0]  # Second element
+    #     if i < 41: continue
+        lang = path[:3]
+        print(lang)
 
-        file_path = os.path.join(ALIGNED_PATH, path)
+        file_path = os.path.join(MONO_PATH, path)
 
         df = pd.read_json(file_path, lines=True)
-        train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+        train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
+        test_df, eval_df = train_test_split(test_df, test_size=0.5, random_state=42)
 
-        src_tgt_list = path.split("-")
-        src = src_tgt_list[1]  # First element
-        tgt = src_tgt_list[2].split(".")[0]  # Second element
-
-        print(f'\n{i} :{src}-{tgt}\n')
-        # Create a DatasetDict
-        dataset_dict = DatasetDict(
-            {
+    #     # Create a DatasetDict
+        dataset_dict =  DatasetDict({
                 "train": Dataset.from_pandas(train_df),
                 "test": Dataset.from_pandas(test_df),
-            }
-        )
-        # dataset_dict.push_to_hub('dsfsi/vukuzenzele-sentence-aligned', f'{src}-{tgt}')
-        path = os.path.join(OUT_PATH / f'{src}-{tgt}' )
-        os.makedirs(path)
-        dataset_dict.save_to_disk(path)
-        # train_df.to_parquet(os.path.join(path, 'train.parquet'))
-        # test_df.to_parquet(os.path.join(path, 'test.parquet'))
+                "eval": Dataset.from_pandas(eval_df),
+            })
+        
+        dataset_dict.push_to_hub('dsfsi/vukuzenzele-monolingual', lang)
+
+
+    # dataset = DatasetDict(big_dict)
+    # dataset.push_to_hub('dsfsi/vukuzenzele-sentence-aligned')
